35f3c0a50d708159afd533807576aa1b123b1d64
Son Chau <sonchau24@yahoo.com>
2020-08-16 10:03:49 +1000

    add basic mapbox with environment variables

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+      MAPBOX_TOKEN: 'pk.eyJ1Ijoic29uY2hhdTI0IiwiYSI6ImNqajNpYnE0NTB3ZnQzcHBkYnM5dDUxNWoifQ.soiwoeUMyJJrm8z_mu66cg',
+    },
+  }
--- pages/basic.tsx
+++ pages/basic.tsx
@@ -1,11 +1,27 @@
-import Layout from '../components/Layout'
 
-const BasicPage = () => {
+import React, {useState} from 'react';
+import MapGL from 'react-map-gl';
+
+const BasicMap = () => {
+    const [viewport, setViewport] = useState({
+        latitude: -37.8112451,
+        longitude: 144.9543962,
+        zoom: 14,
+        bearing: 0,
+        pitch: 0
+      });
+
   return (
-    <Layout title="Basic map">
-      <h1>Basic Map</h1>
-    </Layout>
+      
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+      />
   )
 }
 
-export default BasicPage
+export default BasicMap
